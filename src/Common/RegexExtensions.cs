ef03db9f372215b7c9ccbd4d50d03f492cc0fe5d
scorteanucosmin <cozminsc@gmail.com>
2024-11-22 19:20:47 +0200

    Added source generated regex expressions

---
+++ src/Common/RegexExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Oxide.CompilerServices.Common;
+
+public static partial class RegexExtensions
+{
+    [GeneratedRegex(@"\\[uU]([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+    public static partial Regex UnicodeEscapePattern();
+}
