59bdfeaa761b8eb6326145884addf33bf39ecb70
scorteanucosmin <cozminsc@gmail.com>
2024-11-26 17:15:43 +0200

    Application class cleanup, messaging handling of received messages from the broker

--- src/Application.cs
+++ src/Application.cs
@@ -1,151 +1,165 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using ObjectStream;
-using ObjectStream.Data;
-using Oxide.CompilerServices.Logging;
-using Oxide.CompilerServices.Settings;
+using Oxide.CompilerServices.Common;
+using Oxide.CompilerServices.Enums;
+using Oxide.CompilerServices.Interfaces;
+using Oxide.CompilerServices.Models.Compiler;
+using Oxide.CompilerServices.Models.Configuration;
+using Oxide.CompilerServices.Services;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Oxide.CompilerServices
 {
     internal sealed class Application
     {
-        private IServiceProvider Services { get; }
-        private ILogger Logger { get; }
-        private OxideSettings Settings { get; }
-        private ObjectStreamClient<CompilerMessage>? ObjectStream { get; }
-        private CancellationTokenSource TokenSource { get; }
-        private Queue<CompilerMessage> CompilerQueue { get; }
+        private readonly ILogger _logger;
+        private readonly OxideSettings _settings;
+        private readonly MessageBrokerService _messageBrokerService;
+        private readonly ICompilationService _compilationService;
+        private readonly ISerializer _serializer;
 
-        private Task WorkerTask { get; set; }
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly CancellationToken _cancellationToken;
 
-        public Application(IServiceProvider provider, ILogger<Application> logger, OxideSettings options, CancellationTokenSource tk)
+        private readonly ConcurrentQueue<CompilerMessage> _compilerQueue;
+
+        public Application(MessageBrokerService messageBrokerService, ILogger<Application> logger, OxideSettings options,
+            CancellationTokenSource cancellationTokenSource, ICompilationService compilationService,
+            ISerializer serializer)
         {
             Program.ApplicationLogLevel.MinimumLevel = options.Logging.Level.ToSerilog();
-            TokenSource = tk;
-            Logger = logger;
-            Settings = options;
-            Services = provider;
-            CompilerQueue = new Queue<CompilerMessage>();
 
-            if (!options.Compiler.EnableMessageStream) return;
-
-            ObjectStream = new ObjectStreamClient<CompilerMessage>(Console.OpenStandardInput(), Console.OpenStandardOutput());
-            ObjectStream.Message += (s, m) => OnMessage(m);
+            _messageBrokerService = messageBrokerService;
+            _compilationService = compilationService;
+            _serializer = serializer;
+            _logger = logger;
+            _settings = options;
+            _compilerQueue = new ConcurrentQueue<CompilerMessage>();
+            _cancellationTokenSource = cancellationTokenSource;
+            _cancellationToken = _cancellationTokenSource.Token;
         }
 
-        public void Start()
+        public async ValueTask StartAsync()
         {
-            Logger.LogInformation(Events.Startup, $"Starting compiler v{Assembly.GetExecutingAssembly().GetName().Version}. . .");
-            Logger.LogInformation(Events.Startup, $"Minimal logging level is set to {Program.ApplicationLogLevel.MinimumLevel}");
+            _logger.LogInformation(Constants.StartupEventId, $"Starting compiler v{Assembly.GetExecutingAssembly().GetName().Version}. . .");
+            _logger.LogInformation(Constants.StartupEventId, $"Minimal logging level is set to {Program.ApplicationLogLevel.MinimumLevel}");
+
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             Thread.CurrentThread.Priority = ThreadPriority.BelowNormal;
             Thread.CurrentThread.IsBackground = true;
-            AppDomain.CurrentDomain.ProcessExit += (sender, e) => Exit("SIGTERM");
-            Console.CancelKeyPress += (s, o) => Exit("SIGINT (Ctrl + C)");
+            AppDomain.CurrentDomain.ProcessExit += (_, _) => Stop("SIGTERM");
+            Console.CancelKeyPress += (_, _) => Stop("SIGINT (Ctrl + C)");
 
-            if (Settings.ParentProcess != null)
+            if (_settings.ParentProcess != null)
             {
                 try
                 {
-                    if (!Settings.ParentProcess.HasExited)
+                    if (!_settings.ParentProcess.HasExited)
                     {
-                        Settings.ParentProcess.EnableRaisingEvents = true;
-                        Settings.ParentProcess.Exited += (s, o) => Exit("parent process shutdown");
-                        Logger.LogInformation(Events.Startup, "Watching parent process ([{id}] {name}) for shutdown", Settings.ParentProcess.Id, Settings.ParentProcess.ProcessName);
+                        _settings.ParentProcess.EnableRaisingEvents = true;
+                        _settings.ParentProcess.Exited += (s, o) => Stop("parent process shutdown");
+                        _logger.LogInformation(Constants.StartupEventId, "Watching parent process ([{id}] {name}) for shutdown",
+                            _settings.ParentProcess.Id, _settings.ParentProcess.ProcessName);
                     }
                     else
                     {
-                        Exit("parent process exited");
+                        Stop("parent process exited");
                         return;
                     }
                 }
                 catch (Exception ex)
                 {
-                    Logger.LogWarning(Events.Startup, ex, "Failed to attach to parent process, compiler may stay open if parent is improperly shutdown");
+                    _logger.LogWarning(Constants.StartupEventId, ex, "Failed to attach to parent process, compiler may stay open if parent is improperly shutdown");
                 }
             }
 
-            if (!Settings.Compiler.EnableMessageStream) return;
-
-            Logger.LogDebug(Events.Startup, "Started message server. . .");
-            ObjectStream!.Start();
-            Logger.LogInformation(Events.Startup, "Message server has started");
-            Task.Delay(TimeSpan.FromSeconds(2), TokenSource.Token).Wait();
-            ObjectStream.PushMessage(new CompilerMessage { Type = CompilerMessageType.Ready });
-            Logger.LogInformation(Events.Startup, "Sent ready message to parent process");
-
-            Task<Task> task = Task.Factory.StartNew(
-                function: Worker,
-                cancellationToken: TokenSource.Token,
-                creationOptions: TaskCreationOptions.LongRunning,
-                scheduler: TaskScheduler.Default
-            );
-
-            WorkerTask = task.Unwrap();
-            Logger.LogDebug(Events.Startup, "Compiler has started successfully and is awaiting jobs. . .");
-            WorkerTask.Wait();
-        }
-
-        private void OnMessage(CompilerMessage message)
-        {
-            if (TokenSource.IsCancellationRequested)
+            if (!_settings.Compiler.EnableMessageStream)
             {
                 return;
             }
 
-            switch (message.Type)
-            {
-                case CompilerMessageType.Compile:
-                    lock (CompilerQueue)
-                    {
-                        message.Client = ObjectStream;
-                        CompilerData data = (CompilerData)message.Data;
-                        data.Message = message;
-                        Logger.LogDebug(Events.Compile, $"Received compile job {message.Id} | Plugins: {data.SourceFiles.Length}, References: {data.ReferenceFiles.Length}");
-                        CompilerQueue.Enqueue(message);
-                    }
-                    break;
+            _messageBrokerService.Start(Console.OpenStandardOutput(), Console.OpenStandardInput());
 
-                case CompilerMessageType.Exit:
-                    Exit("compiler stream");
-                    break;
+            _messageBrokerService.OnMessageReceived += OnMessageReceived;
+
+            _messageBrokerService.SendReadyMessage();
+
+            await Task.Delay(TimeSpan.FromSeconds(2), _cancellationToken);
+
+            _logger.LogDebug(Constants.StartupEventId, "Compiler has started successfully and is awaiting jobs. . .");
+
+            try
+            {
+                await WorkerAsync();
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogDebug(Constants.ShutdownEventId, "Worker has been cancelled");
             }
         }
 
-        private async Task Worker()
+        private void OnMessageReceived(CompilerMessage compilerMessage)
         {
-            CompilerMessage message = null;
-            while (!TokenSource.IsCancellationRequested)
+            _logger.LogInformation($"Received message from client of type: {compilerMessage.Type}");
+            if (_cancellationToken.IsCancellationRequested)
             {
+                return;
+            }
 
-                lock (CompilerQueue)
+            switch (compilerMessage.Type)
+            {
+                case MessageType.Data:
                 {
-                    if (CompilerQueue.Count != 0)
-                    {
-                        message = CompilerQueue.Dequeue();
-                    }
-                    else
-                    {
-                        message = null;
-                    }
+                    CompilerData compilerData = _serializer.Deserialize<CompilerData>(compilerMessage.Data);
+
+                    _logger.LogDebug(Constants.CompileEventId,
+                        $"Received compile job {compilerMessage.Id} | Plugins: {compilerData.SourceFiles.Length}, References: {compilerData.ReferenceFiles.Length}");
+
+                    _compilerQueue.Enqueue(compilerMessage);
+                    break;
                 }
-
-                if (message == null)
+                case MessageType.Heartbeat:
                 {
-                    await Task.Delay(1000);
+                    //_logger.LogDebug(Constants.HeartbeatEventId, "Received heartbeat from client");
+                    break;
+                }
+                case MessageType.Shutdown:
+                {
+                    Stop("compiler stream");
+                    break;
+                }
+            }
+        }
+
+        private async ValueTask WorkerAsync()
+        {
+            while (!_cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(500, _cancellationToken);
+                if (!_compilerQueue.TryDequeue(out CompilerMessage compilerMessage))
+                {
                     continue;
                 }
 
-                CompilerData data = (CompilerData)message.Data;
-                ICompilerService compiler = Services.GetRequiredService<ICompilerService>();
-                await compiler.Compile(message.Id, data);
+                try
+                {
+                    CompilerData data = _serializer.Deserialize<CompilerData>(compilerMessage.Data);
+
+                    await _compilationService.Compile(compilerMessage.Id, data);
+
+                    _logger.LogInformation($"Completed compile job {compilerMessage.Id}");
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogError($"Error occurred while compiling job {compilerMessage.Id}: {exception}");
+                    throw;
+                }
             }
         }
 
-        private void Exit(string? source)
+        private void Stop(string? source)
         {
             string message = "Termination request has been received";
             if (!string.IsNullOrWhiteSpace(message))
@@ -153,8 +167,12 @@ namespace Oxide.CompilerServices
                 message += $" from {source}";
             }
 
-            Logger.LogInformation(Events.Shutdown, message);
-            TokenSource.Cancel();
+            _logger.LogInformation(Constants.ShutdownEventId, message);
+
+            _cancellationTokenSource.Cancel();
+
+            _messageBrokerService.OnMessageReceived -= OnMessageReceived;
+            _messageBrokerService.Stop();
         }
     }
 }
