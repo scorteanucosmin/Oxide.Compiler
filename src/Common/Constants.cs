5d929ed35f80618064ed73cde77e6ac6d71eaaea
scorteanucosmin <cozminsc@gmail.com>
2024-11-26 17:10:07 +0200

    Moved logging event ids to Constants

---
+++ src/Common/Constants.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Oxide.CompilerServices.Common;
+
+public static class Constants
+{
+    public static readonly EventId StartupEventId = new(1, "Startup");
+    public static readonly EventId ShutdownEventId = new(2, "Shutdown");
+
+    public static readonly EventId CommandEventId = new(3, "Command");
+    public static readonly EventId CompileEventId = new(4, "Compile");
+}
