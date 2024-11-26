79e775000387148df6dba7aedc10a21c47eb0871
scorteanucosmin <cozminsc@gmail.com>
2024-11-26 16:57:39 +0200

    App entry point cleanup & asynchronous start, dependency injecting message broker service

--- src/Program.cs
+++ src/Program.cs
@@ -3,53 +3,58 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Oxide.CompilerServices.CSharp;
-using Oxide.CompilerServices.Settings;
+using Oxide.CompilerServices.CompilerStream;
+using Oxide.CompilerServices.Interfaces;
+using Oxide.CompilerServices.Models.Configuration;
+using Oxide.CompilerServices.Services;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
 
-namespace Oxide.CompilerServices
+namespace Oxide.CompilerServices;
+
+internal class Program
 {
-    internal class Program
-    {
 #if DEBUG
         public static LoggingLevelSwitch ApplicationLogLevel { get; } = new(LogEventLevel.Debug);
         public const bool DEBUG = true;
 #else
-        public static LoggingLevelSwitch ApplicationLogLevel { get; } = new(LogEventLevel.Information);
-        public const bool DEBUG = false;
+    public static LoggingLevelSwitch ApplicationLogLevel { get; } = new(LogEventLevel.Information);
+    public const bool DEBUG = false;
 #endif
 
-        public static void Main(string[] args) => new ApplicationBuilder()
-            .WithConfiguration((config) => config.AddCommandLine(args, new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase)
+    public static async Task Main(string[] args)
+    {
+        Application application = new ApplicationBuilder().WithConfiguration(config => config.AddCommandLine(args,
+                    new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase)
+                    {
+                        ["-l:file"] = "Logging:FileName",
+                        ["-v"] = "Logging:Level",
+                        ["--logging"] = "Logging",
+                        ["--verbose"] = "Logging:Level",
+
+                        ["-unsafe"] = "Compiler:AllowUnsafe",
+                        ["-std"] = "Compiler:UseStandardLibraries",
+                        ["-ms"] = "Compiler:EnableMessageStream",
+                        ["--setting"] = "Compiler",
+
+                        ["--path"] = "Path",
+                        ["--parent"] = "MainProcess"
+                    })
+                .AddJsonFile(Path.Combine(AppContext.BaseDirectory, "oxide.compiler.json"), true)
+                .AddEnvironmentVariables("Oxide_"), (configuration, services) =>
             {
-                ["-l:file"]     = "Logging:FileName",
-                ["-v"]          = "Logging:Level",
-                ["--logging"]   = "Logging",
-                ["--verbose"]   = "Logging:Level",
+                services.Configure<LogSettings>(configuration.GetSection("Logging"))
+                    .AddScoped(cfg => cfg.GetRequiredService<IOptions<LogSettings>>().Value);
 
-                ["-unsafe"]     = "Compiler:AllowUnsafe",
-                ["-std"]        = "Compiler:UseStandardLibraries",
-                ["-ms"]         = "Compiler:EnableMessageStream",
-                ["--setting"]   = "Compiler",
+                services.Configure<CompilerSettings>(configuration.GetSection("Compiler"))
+                    .AddScoped(cfg => cfg.GetRequiredService<IOptions<CompilerSettings>>().Value);
 
-                ["--path"]      = "Path",
-                ["--parent"]    = "MainProcess"
-            })
-            .AddJsonFile(Path.Combine(AppContext.BaseDirectory, "oxide.compiler.json"), true)
-            .AddEnvironmentVariables("Oxide_"), (c, s) =>
-            {
-                s.Configure<LogSettings>(c.GetSection("Logging"))
-                .AddScoped(cfg => cfg.GetRequiredService<IOptions<LogSettings>>().Value);
-                s.Configure<CompilerSettings>(c.GetSection("Compiler"))
-                .AddScoped(cfg => cfg.GetRequiredService<IOptions<CompilerSettings>>().Value);
-                s.Configure<DirectorySettings>(c.GetSection("Path"))
-                .AddScoped(cfg => cfg.GetRequiredService<IOptions<DirectorySettings>>().Value);
+                services.Configure<DirectorySettings>(configuration.GetSection("Path"))
+                    .AddScoped(cfg => cfg.GetRequiredService<IOptions<DirectorySettings>>().Value);
 
-                s.AddSingleton<OxideSettings>();
-            })
-            .WithLogging((logging, cfg) =>
+                services.AddSingleton<OxideSettings>();
+            }).WithLogging((logging, cfg) =>
             {
                 IConfigurationSection logSettings = cfg.GetSection("Logging");
                 string filePath = logSettings.GetValue("FileName", "oxide.compiler.log");
@@ -75,6 +80,7 @@ namespace Oxide.CompilerServices
                         retainedFileCountLimit: 5,
                         shared: true)
                     .CreateLogger();
+
                 logging.AddSerilog(null, true);
 #if DEBUG
                 logging.AddDebug();
@@ -83,11 +89,14 @@ namespace Oxide.CompilerServices
                 {
                     logging.AddSimpleConsole();
                 }
-            })
-            .WithServices(s => s.AddSingleton<CancellationTokenSource>()
-            .AddSingleton<ICompilerService, CSharpLanguage>()
-            .AddTransient<MetadataReferenceResolver, OxideResolver>())
-            .Build()
-            .Start();
+            }).WithServices(services => services
+                .AddSingleton<CancellationTokenSource>()
+                .AddSingleton<ICompilationService, CompilationService>()
+                .AddTransient<MetadataReferenceResolver, OxideResolver>()
+                .AddSingleton<MessageBrokerService>()
+                .AddSingleton<ISerializer, Serializer>())
+            .Build();
+
+        await application.StartAsync();
     }
 }
