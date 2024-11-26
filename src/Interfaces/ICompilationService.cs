a6ec25c79dc48b41a914aa7ef8bfc072b7a605a2
scorteanucosmin <cozminsc@gmail.com>
2024-11-26 17:07:20 +0200

    Moved interfaces to Interfaces directory

---
+++ src/Interfaces/ICompilationService.cs
@@ -0,0 +1,8 @@
+﻿using Oxide.CompilerServices.Models.Compiler;
+
+namespace Oxide.CompilerServices.Interfaces;
+
+public interface ICompilationService
+{
+    public Task Compile(int id, CompilerData data);
+}
