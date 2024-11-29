2a8fedadbb878f4a000104c2bf8e9ebb5a8af7a3
scorteanucosmin <cozminsc@gmail.com>
2024-11-29 04:28:36 +0200

    Regex extensions changes

--- src/Common/RegexExtensions.cs
+++ src/Common/RegexExtensions.cs
@@ -4,6 +4,6 @@ namespace Oxide.CompilerServices.Common;
 
 public static partial class RegexExtensions
 {
-    [GeneratedRegex(@"\\[uU]([0-9A-F]{4})", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
-    public static partial Regex UnicodeEscapePattern();
+    [GeneratedRegex(@"\\[uU]([0-9A-F]{4})", RegexOptions.IgnoreCase, "en-US")]
+    public static partial Regex UnicodeEscapePattern { get; }
 }
